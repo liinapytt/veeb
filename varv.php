240300f6c720a6061a9af3b3e3be83690d3f69cd
Liina Püttsepp <liina.rebasssoo@khk.ee>
2017-12-14 14:32:19 +0200

    funktsioon do ja while käsitlemine

--- varv.php
+++ varv.php
@@ -35,3 +35,19 @@ echo 'b = '.$b.'<br />';
 echo 'c = '.$c.'<br />';
 echo 'd = '.$d.'<br />';
 //
+$kord = 1;
+while($kord <= 5){
+    echo '<i>'.$kord.'</i><br />';
+    if($kord == 3){
+        break;
+    }
+    echo '<b>'.$kord.++'</b><br />';
+}
+//
+$kord = 1;
+do {
+    if ($kord == 3 ){
+        continue;
+    }
+    echo '<b>'.$kord.'</b><br />';
+}while(++$kord <= 5);

e39cc56b9ab9b3b85894d8edf6078d29e9a35712
Liina Püttsepp <liina.rebasssoo@khk.ee>
2017-12-14 14:52:36 +0200

    Koostatud tabeli väljastus funktsioon

---
+++ funktsioonid.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liinareb
+ * Date: 14.12.2017
+ * Time: 14:33
+ */
+function tabel ($sona1, $sona2, $sona3, $sona4){
+    echo '<table border="1">';
+         for($rida = 1; $rida <= 4; $rida++){
+            echo '<tr>';
+                echo '<td>';
+                // muutuja genereerimine ${}
+                    echo ${'sona'}.$rida;
+                echo '</td>';
+            echo '</tr>';
+    }
+    echo '</table>';
+}
+tabel('1', '2', '3', '4');

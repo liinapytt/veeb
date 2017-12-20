031091e9565d8e695c7390c5c0ed8ce652c29314
Liina Püttsepp <liina.rebasssoo@khk.ee>
2017-12-20 22:12:02 +0200

    Numbri arvude summa funktsiooni loomine

---
+++ kodu2.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liinareb
+ * Date: 20.12.2017
+ * Time: 21:38
+ */
+/* 2. Koostada funktsioon nimega otsiNumber, mis võtab parameetrina suvalise arvu ja kindlaks määratud arvu ja arvutab mitu korda määratud arv esineb suvalises numbris, näiteks arv 5 esineb numbris 442158755745 neli korda.  Lahendamiseks ei tohi kasutada sõnetöötluse võimalused*/
+
+function otsiNumber($arv, $kindelArv){
+    $kindelArv = 3;
+    while ($arv != 0){
+
+    }
+
+}

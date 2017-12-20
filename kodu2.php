1cae27d63fd37ae61752f67bc9f2a438e261bca1
Liina Püttsepp <liina.rebasssoo@khk.ee>
2017-12-20 22:29:55 +0200

    Kindla arvu esinemissageduse suvalises numbris funktsiooni loomine

--- kodu2.php
+++ kodu2.php
@@ -7,10 +7,18 @@
  */
 /* 2. Koostada funktsioon nimega otsiNumber, mis võtab parameetrina suvalise arvu ja kindlaks määratud arvu ja arvutab mitu korda määratud arv esineb suvalises numbris, näiteks arv 5 esineb numbris 442158755745 neli korda.  Lahendamiseks ei tohi kasutada sõnetöötluse võimalused*/
 
-function otsiNumber($arv, $kindelArv){
-    $kindelArv = 3;
-    while ($arv != 0){
-
+function otsiNumber($suvaArv, $kindelArv){
+    echo 'Arv '.$kindelArv.' esineb arvus '.$suvaArv;
+    $arvKordi = 0;
+    while ($suvaArv != 0){
+        $arv = $suvaArv % 10;
+        if($arv == $kindelArv){
+            $arvKordi++;
+        }
+        $suvaArv = $suvaArv / 10;
+        settype($suvaArv, 'integer');
     }
+    echo '  '.$arvKordi.'  korda<br />';
 
 }
+otsiNumber(679234732, 3);
